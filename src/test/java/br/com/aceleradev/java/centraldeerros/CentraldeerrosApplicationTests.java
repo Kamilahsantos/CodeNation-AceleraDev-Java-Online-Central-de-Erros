7e2e4f6ef6d33bd8482cc7ef00ee30dc492fbeaa
kamilah santos <kahsantos89@gmail.com>
2020-06-03 02:18:14 -0300

    criacao da aplicacao

---
+++ src/test/java/br/com/aceleradev/java/centraldeerros/CentraldeerrosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aceleradev.java.centraldeerros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentraldeerrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
