49a74cca9bc7516f288f6deb5bd4aa51a7144f2e
kamilah santos <kahsantos89@gmail.com>
2020-06-20 12:30:18 -0300

    configuracoes inicias de seguranca e cors

--- src/main/java/br/com/aceleradev/java/centraldeerros/Auth/JWTAuth.java
+++ src/main/java/br/com/aceleradev/java/centraldeerros/Auth/JWTAuth.java
@@ -1,4 +1,11 @@
 package br.com.aceleradev.java.centraldeerros.Auth;
 
-public class JWTAuth {
+import javax.servlet.*;
+import java.io.IOException;
+
+public class JWTAuth implements Filter {
+  @Override
+  public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+  }
 }
